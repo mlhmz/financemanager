083fa7d15c5feb37d991afd50e02946c97ef0a9c
Malek Hamza <malek.hamza@gmx.de>
2023-09-07 21:34:35 +0200

    feat: Add category controller

---
+++ src/main/java/xyz/mlhmz/financemanager/controllers/CategoryController.java
@@ -0,0 +1,54 @@
+package xyz.mlhmz.financemanager.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import xyz.mlhmz.financemanager.dtos.MutateCategoryDto;
+import xyz.mlhmz.financemanager.dtos.QueryCategoryDto;
+import xyz.mlhmz.financemanager.entities.Category;
+import xyz.mlhmz.financemanager.mappers.CategoryMapper;
+import xyz.mlhmz.financemanager.services.CategoryService;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/categories")
+@AllArgsConstructor
+public class CategoryController {
+    private final CategoryService categoryService;
+    private final CategoryMapper categoryMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public QueryCategoryDto createCategory(@RequestBody MutateCategoryDto mutateCategoryDto) {
+        Category mappedCategory = this.categoryMapper.mapMutateCategoryDtoToCategory(mutateCategoryDto);
+        Category category = categoryService.createCategory(mappedCategory);
+        return this.categoryMapper.mapCategoryToQueryCategoryDto(category);
+    }
+
+    @GetMapping("/{uuid}")
+    public QueryCategoryDto findCategoryByUuid(@PathVariable UUID uuid) {
+        Category category = this.categoryService.findCategoryByUUID(uuid);
+        return this.categoryMapper.mapCategoryToQueryCategoryDto(category);
+    }
+
+    @GetMapping
+    public List<QueryCategoryDto> findAllCategories() {
+        List<Category> categories = this.categoryService.findAllCategories();
+        return this.categoryMapper.mapCategoryListToQueryCategoryList(categories);
+    }
+
+    @PutMapping("/{uuid}")
+    public QueryCategoryDto updateCategory(@PathVariable UUID uuid, MutateCategoryDto mutateCategoryDto) {
+        Category newCategory = this.categoryMapper.mapMutateCategoryDtoToCategory(mutateCategoryDto);
+        Category category = this.categoryService.updateCategory(uuid, newCategory);
+        return this.categoryMapper.mapCategoryToQueryCategoryDto(category);
+    }
+
+    @DeleteMapping("/{uuid}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCategoryByUUID(@PathVariable UUID uuid) {
+        this.categoryService.deleteCategoryByUuid(uuid);
+    }
+}
