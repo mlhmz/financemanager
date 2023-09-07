2e6cbf8dbd8bf8de941f2f0c295a846b2024168d
Malek Hamza <malek.hamza@gmx.de>
2023-09-07 21:12:05 +0200

    feat: Add actual functionality to the mapper

--- src/main/java/xyz/mlhmz/financemanager/mappers/CategoryMapper.java
+++ src/main/java/xyz/mlhmz/financemanager/mappers/CategoryMapper.java
@@ -1,7 +1,12 @@
 package xyz.mlhmz.financemanager.mappers;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import xyz.mlhmz.financemanager.entities.Category;
 
+@Mapper(
+        componentModel = "spring"
+)
 public interface CategoryMapper {
-    void updateCategory(Category existingCategory, Category newCategory);
+    void updateCategory(@MappingTarget Category existingCategory, Category newCategory);
 }
