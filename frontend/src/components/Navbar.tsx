d8ec0a513763908c4a4c92c430cc281137acd7a7
Malek Hamza <malek.hamza@gmx.de>
2023-09-15 10:43:22 +0200

    chore: Clean up nav

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -9,23 +9,7 @@ export const Navbar = () => {
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>Link</a>
-          </li>
-          <li>
             <ThemeSelect />
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2 bg-primary">
-                <li>
-                  <a>Link 1</a>
-                </li>
-                <li>
-                  <a>Link 2</a>
-                </li>
-              </ul>
-            </details>
           </li>
         </ul>
       </div>
