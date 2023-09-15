57ec7310d7e6fccf36c565458ed741d854754aa2
Malek Hamza <malek.hamza@gmx.de>
2023-09-15 11:07:23 +0200

    chore: Add Providers and DevTools for React Query

--- frontend/src/Providers.tsx
+++ frontend/src/Providers.tsx
@@ -1,7 +1,12 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 
+const queryClient = new QueryClient();
+
 export const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <>
-    { children }
-  </>
-}
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </>
+  );
+};
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { Providers } from "./Providers.tsx";
 import { RouterProvider } from "react-router-dom";
 import router from "./router.tsx";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <App>
         <RouterProvider router={router} />
       </App>
+      <ReactQueryDevtools initialIsOpen={false} />
     </Providers>
   </React.StrictMode>
 );
