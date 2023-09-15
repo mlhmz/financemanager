61cdaaf62b3aee0b661c6fd3f8aff4f14a9ec4f1
Malek Hamza <malek.hamza@gmx.de>
2023-09-15 16:43:09 +0200

    fix: clicking on the nav title wont lead to main page

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { AuthButton } from "./AuthButton";
 import { ThemeSelect } from "./ThemeSelect";
 
@@ -5,7 +6,7 @@ export const Navbar = () => {
   return (
     <div className="navbar bg-base-200 z-10 bg-opacity-80 fixed backdrop-blur-sm">
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">Finance Manager</a>
+        <Link to="/" className="btn btn-ghost normal-case text-xl">Finance Manager</Link>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
