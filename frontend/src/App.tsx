cb87be7cc0134fb73011f2a57deaf2d78fc31d1b
Malek Hamza <malek.hamza@gmx.de>
2023-09-15 07:43:23 +0200

    fix: daisyui not working

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 function App() {
 
   return (
-    <>
+    <div>
       <h1>Vite + React</h1>
-    </>
+      <button className="btn btn-primary">Button</button>
+    </div>
   )
 }
 
---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,11 @@
+import daisyui from 'daisyui';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+}
+
