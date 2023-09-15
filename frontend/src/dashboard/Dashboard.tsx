012f485d6a9c38d28a6bbb2411aaa2e116acfa1f
Malek Hamza <malek.hamza@gmx.de>
2023-09-15 14:33:48 +0200

    chore: Improve dashboard card styling

--- frontend/src/dashboard/Dashboard.tsx
+++ frontend/src/dashboard/Dashboard.tsx
@@ -7,15 +7,14 @@ export const Dashboard = () => {
     <div className="container m-auto">
       <h1 className="text-2xl">Hi, {auth.user?.profile.given_name}!</h1>
       <p>What do you want to do?</p>
-
-      <div className="flex justify-between my-5">
-        <div className="card w-96 bg-primary text-primary-content">
+      <div className="flex gap-3 my-5">
+        <div className="card w-96 bg-neutral hover:bg-neutral-focus text-neutral-content cursor-pointer">
           <div className="card-body">
             <h2 className="card-title">Categories</h2>
             <p>Manage your categories</p>
           </div>
         </div>
-        <div className="card w-96 bg-primary text-primary-content">
+        <div className="card w-96 bg-neutral hover:bg-neutral-focus text-neutral-content cursor-pointer">
           <div className="card-body">
             <h2 className="card-title">Sheets</h2>
             <p>See and manage your sheets</p>
