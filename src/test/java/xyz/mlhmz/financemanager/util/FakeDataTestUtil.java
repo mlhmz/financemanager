3e86652e0772450facb9d5a92f2a3aceb4444121
Malek Hamza <malek.hamza@gmx.de>
2024-05-09 19:22:32 +0200

    fix: replace weird faker code with javafaker library

--- src/test/java/xyz/mlhmz/financemanager/util/FakeDataTestUtil.java
+++ src/test/java/xyz/mlhmz/financemanager/util/FakeDataTestUtil.java
@@ -1,54 +1,32 @@
 package xyz.mlhmz.financemanager.util;
 
+import com.github.javafaker.Faker;
 import org.jetbrains.annotations.Nullable;
 import xyz.mlhmz.financemanager.entities.Category;
 import xyz.mlhmz.financemanager.entities.Sheet;
 import xyz.mlhmz.financemanager.entities.Transaction;
 
-import java.util.Random;
-
 public class FakeDataTestUtil {
-    private static final String[] RANDOM_WORD_LIST = new String[]{
-            "teacher", "revolution", "ability", "depth", "perception", "inspector", "diamond", "introduction", "basket",
-            "breath", "fortune", "food", "data", "ear", "vehicle", "housing", "criticism", "freedom", "bonus",
-            "employee", "quality", "perspective", "disaster", "office", "magazine", "bathroom", "piano", "outcome",
-            "passenger", "efficiency", "safety", "platform", "thing", "college", "coffee", "obligation", "homework",
-            "device", "championship", "industry", "suggestion", "road", "resolution", "sir", "flight"
-    };
-
-
     private FakeDataTestUtil() {
     }
 
     public static Sheet createFakeSheet() {
-        return Sheet.builder().title(getRandomWord()).build();
+        Faker faker = new Faker();
+        return Sheet.builder().title(faker.animal().name()).build();
     }
 
     public static Category createFakeCategory() {
-        return Category.builder().title(getRandomWord()).description(getRandomSentence()).build();
+        Faker faker = new Faker();
+        return Category.builder().title(faker.animal().name()).description(faker.lorem().sentence()).build();
     }
 
     public static Transaction createFakeTransaction(@Nullable Double amount) {
+        Faker faker = new Faker();
         return Transaction.builder()
-                .title(getRandomWord())
-                .description(getRandomSentence())
-                .amount(amount == null ? new Random().nextDouble(500) - new Random().nextDouble(500) : amount)
+                .title(faker.animal().name())
+                .description(faker.lorem().sentence())
+                .amount(amount != null ? amount : faker.random().nextDouble())
                 .build();
     }
 
-    private static String getRandomWord() {
-        Random random = new Random();
-        int word = random.nextInt(RANDOM_WORD_LIST.length);
-        return RANDOM_WORD_LIST[word - 1];
-    }
-
-    private static String getRandomSentence() {
-        Random random = new Random();
-        int sentenceLength = random.nextInt(10);
-        String[] sentence = new String[sentenceLength];
-        for (int i = 0; i < sentenceLength; i++) {
-            sentence[i] = getRandomWord();
-        }
-        return String.join(" ", sentence);
-    }
 }
