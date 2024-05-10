c92458654c88e3c01d6c14f97fb2674fc2c9a558
Malek Hamza <malek.hamza@gmx.de>
2024-05-10 13:06:37 +0200

    feat: change graphql path to /api/graphql

--- frontend/src/hooks/use-auth-graph-ql-client.tsx
+++ frontend/src/hooks/use-auth-graph-ql-client.tsx
@@ -4,7 +4,7 @@ import { useAuth } from "react-oidc-context";
 export const useAuthGraphQlClient = () => {
 	const auth = useAuth();
 	const client = new GraphQLClient(
-		window.location.protocol + "//" + window.location.host + "/graphql",
+		window.location.protocol + "//" + window.location.host + "/api/graphql",
 		{
 			headers: {
 				Authorization: `Bearer ${auth.user?.access_token}`,
